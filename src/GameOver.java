237ed2bc90455a75d54738b05d82e783d7e73c1e
treetree0211 <yueqiaozhang79@gmail.com>
2019-11-15 00:03:34 -0800

    Added basic class object for scoreboard composite design pattern

--- src/GameOver.java
+++ src/GameOver.java
@@ -11,14 +11,20 @@ public class GameOver extends World
     int score;
     /**
      * Constructor for objects of class GameOver.
-     * 
+     *
      */
+    private ScoreBoardComponent scoreboardComponent;
+
     public GameOver(int counter)
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(560, 560, 1); 
-        score=counter;
-        showText("GAME OVER\n\n Score = "+score+"\n\nPress n for new game", getWidth()/2, getHeight()/2);
+        String input = Greenfoot.ask("What is your name ?");
+        scoreboardComponent = new ScoreBoardComposite();
+        //System.out.println(input);
+        score = counter;
+        prepare();
+        //showText("Sorry " + input +" GAME OVER\n\n Score = "+score+"\n\nPress n for new game", getWidth()/2, getHeight()/2);
         
     }
     public void act()
@@ -28,5 +34,18 @@ public class GameOver extends World
            YarnWorld yarnworld = new YarnWorld();
            Greenfoot.setWorld(yarnworld);
         }
+    }
+
+    private void prepare() {
+        ScoreBoardComponent composite = new ScoreBoardComposite();
+        ScoreBoardComponent name = new UserName();
+        ScoreBoardComponent userScore = new Score(score, 100, 100);
+        addObject(composite, 200, 200);
+        addObject(name, 0, 0);
+        addObject(userScore, 0, 0);
+        composite.addChild(name);
+        composite.addChild(userScore);
+        scoreboardComponent.addChild(composite);
+        scoreboardComponent.printDescription();
     }
 }
---
+++ src/ScoreBoardComposite.java
@@ -0,0 +1,33 @@
+import greenfoot.*;
+import java.util.*;
+/**
+ * @author Yueqiao
+ * @version 1.0 Nov 14
+ */
+
+public class ScoreBoardComposite extends ScoreBoardComponent {
+
+	private ArrayList<ScoreBoardComponent> components = new ArrayList<>();
+
+	public ScoreBoardComposite() {
+
+	}
+
+    public void printDescription() {
+    	for (ScoreBoardComponent obj : components) {
+    		obj.printDescription();
+    	}
+    }
+
+    public void addChild(ScoreBoardComponent c) {
+    	components.add(c);
+    }
+
+    public void removeChild(ScoreBoardComponent c){
+    	components.remove(c);
+    }
+
+    public ScoreBoardComponent getChild(int i){
+    	return components.get(i);
+    }
+}
