6cc2056aa63eaff9ec78623d90a4f06391c7d99e
treetree0211 <yueqiaozhang79@gmail.com>
2019-11-16 17:45:30 -0800

    Added button and entryworld

---
+++ src/GameEntryWorld.java
@@ -0,0 +1,42 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author Yueqiao	
+ * @version Nov 16
+ */
+public class GameEntryWorld extends World
+{	
+	private GreenfootImage background;
+	private YarnWorld yw;
+	private StartButton sb;
+
+	public GameEntryWorld() {
+		super(750, 600, 1);
+		sb = new StartButton();
+
+		background = new GreenfootImage("entry.png");
+		background.scale(getWidth(), getHeight());
+		setBackground(background);
+		addObject(sb, 400, 500);
+		yw = new YarnWorld();
+	}
+
+	public void act() {
+		if (Greenfoot.mouseClicked(sb)) {
+			Greenfoot.setWorld(yw);
+		}
+	}
+
+	private class StartButton extends Actor {
+		private GreenfootImage buttonImage;
+
+		public StartButton() {
+			buttonImage = new GreenfootImage("start.png");
+			buttonImage.scale(200, 80);
+			setImage(buttonImage);
+		}
+	}
+
+}
---
+++ src/ScoreBoardButton.java
@@ -0,0 +1,35 @@
+import greenfoot.*;
+/**
+ * @author Yueqiao
+ * @version 1.0 Nov 14
+ */
+
+public class ScoreBoardButton extends ScoreBoardComponent {
+
+    private int x;
+    private int y;
+    private GreenfootImage buttonImage;
+
+    public ScoreBoardButton() {
+        // this.x = x;
+        // this.y = y;
+        buttonImage = new GreenfootImage("replay.png");
+        buttonImage.scale(200, 50);
+        setImage(buttonImage);
+    }
+
+	public void printDescription() {
+    	//getWorld().showText(name, x, y);
+    }
+
+    public void addChild(ScoreBoardComponent c) {
+ 
+    }
+
+    public void removeChild(ScoreBoardComponent c){
+    }
+
+    public ScoreBoardComponent getChild(int i){
+    	return null;
+    }
+}
